ae586426311da1f255cada332d511a9d4eea1ee2
Sriram Keerthi M K <sriram@raremile.com>
2015-12-12 10:09:05 +0530

    Initial commit with some pom fixes

---
+++ hermes/hermesapp/src/main/scala/com/caffinc/hermes/app/App.scala
@@ -0,0 +1,10 @@
+package com.caffinc.hermes.app
+
+/**
+  * Hello world!
+  *
+  */
+object App extends App {
+  println("Hermes v0.1")
+  println("Personalized Corporate Email Sender")
+}
---
+++ hermes/hermesapp/src/test/scala/com/caffinc/hermes/app/AppTest.scala
@@ -0,0 +1,11 @@
+package com.caffinc.hermes.app
+
+import org.scalatest.FlatSpec
+
+class AppTest extends FlatSpec {
+  "The App" should "start" in {
+    App.main(new Array[String](0))
+  }
+}
+
+
